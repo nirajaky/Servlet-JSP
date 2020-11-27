db70353bfdb2994ea88253ca0c9b3855d92187e0
Niraj Mehta <niraj86995@gmail.com>
2020-11-27 13:32:37 +0530

    Calling a Servlet From a Servlet
    The RequestDispatcher interface provides the facility of dispatching the request to another resource it may be html, servlet or jsp. This interface can also be used to include the content of another resource also. It is one of the way of servlet collaboration.
    
    There are two methods defined in the RequestDispatcher interface.
    
    
    Methods of RequestDispatcher interface
    The RequestDispatcher interface provides two methods. They are:
    
    
    public void forward(ServletRequest request,ServletResponse response)throws ServletException,java.io.IOException:Forwards a request from a servlet to another resource (servlet, JSP file, or HTML file) on the server.
    public void include(ServletRequest request,ServletResponse response)throws ServletException,java.io.IOException:Includes the content of a resource (servlet, JSP page, or HTML file) in the response.

---
+++ RequestDispatcher/src/com/nirajaky/SqServlet.java
@@ -0,0 +1,18 @@
+package com.nirajaky;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet{
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int k = (int)req.getAttribute("k");    // to receive value from another servlet
+		k = k*k;
+		PrintWriter out = res.getWriter();
+		out.println("Square is " + k);
+	}
+}
