ce9adeb1f380075e1c78b6d51eb2f74d01eb051b
Niraj Mehta <niraj86995@gmail.com>
2020-11-28 00:46:48 +0530

    Tomcat7+ version only/-
    Annotation represents the metadata. If you use annotation, deployment descriptor (web.xml file) is not required. But you should have tomcat7 as it will not run in the previous versions of tomcat. @WebServlet annotation is used to map the servlet with the specified name.

---
+++ Annotation_Servlet/src/com/nirajaky/AddServlet.java
@@ -0,0 +1,31 @@
+package com.nirajaky;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/add")
+public class AddServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		int a = Integer.parseInt(req.getParameter("num1"));
+		int b = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = a+b;
+		System.out.println(k);
+		
+		req.setAttribute("k", k);      // to send data to another Servlet
+		
+		//Calling Another Servlet from current Servlet
+		RequestDispatcher rd = req.getRequestDispatcher("sq");  // sq : is url Pattern of another Servlet
+		rd.forward(req, res);
+		
+		PrintWriter out = res.getWriter();
+		out.println("Result is " + k);
+	}
+}
